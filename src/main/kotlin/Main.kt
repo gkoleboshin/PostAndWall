7deb80e6532e12d1719f714fbb316408b822daff
Gennadiy Koleboshin <gk4521@gmail.ru>
2022-01-17 01:50:38 +0300

    bad test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -34,7 +34,7 @@ object WallService{
             postWithId = post.copy(id= post.id+1)
             havePosts=true
         }else{
-            postWithId = post.copy(id= posts.last().id+1)
+            postWithId = post.copy()
         }
         posts+= postWithId
         return posts.last()
