473df8f4b5a1c502873941f9b1940b7b199e9b2e
Gennadiy Koleboshin <gk4521@gmail.ru>
2022-01-22 18:00:00 +0300

    Задание 2.3.2 исправил автотесты

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -3,23 +3,30 @@ import org.junit.Test
 import org.junit.Assert.*
 import ru.netology.*
 import ru.netology.post.*
+import ru.netology.post.atachments.Atachment
+import ru.netology.post.atachments.AtachmentVideo
+import ru.netology.post.multimediaContent.Video
 
 class WallServiceTest {
 
     @Test
     fun update_existing() {
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
+        var atachments:Array<Atachment> = emptyArray<Atachment>()
+        atachments+=AtachmentVideo(Video())
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
         val resault = WallService.editPost(Post(2,0,0,0,
-            getDate(),"HelloWorld 2022!",0,false, null, null, null, null,null,"_",0,false,false,false,false,false,false, null,0))
+            getDate(),"HelloWorld 2022!",0,false, null, null, null, null,null,"_",0,false,false,false,false,false,false, null,0,atachments))
         assertTrue(resault)
         WallService.posts = emptyArray()
     }
 
     @Test
     fun add() {
-        val expendetPost = Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0)
+        var atachments:Array<Atachment> = emptyArray<Atachment>()
+        atachments+=AtachmentVideo(Video())
+        val expendetPost = Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments)
         val actualPost = WallService.add(expendetPost)
         assertFalse(expendetPost.id == actualPost.id)
         WallService.posts = emptyArray()
@@ -27,11 +34,13 @@ class WallServiceTest {
 
     @Test
     fun update_is_not_existing() {
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
-        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0))
+        var atachments:Array<Atachment> = emptyArray<Atachment>()
+        atachments+=AtachmentVideo(Video())
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
+        WallService.add(Post(0,0,0,0,getDate(),"HelloWorld!",0,false, null, null, null, null,null, " ",0,false,false,false,false,false,false, null,0,atachments))
         val resault = WallService.editPost(Post(4,0,0,0,
-            getDate(),"HelloWorld 2022!",0,false, null, null, null, null,null,"_",0,false,false,false,false,false,false, null,0))
+            getDate(),"HelloWorld 2022!",0,false, null, null, null, null,null,"_",0,false,false,false,false,false,false, null,0,atachments))
         assertFalse(resault)
         WallService.posts = emptyArray()
     }
